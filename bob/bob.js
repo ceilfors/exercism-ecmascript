e49cddc4293e530b5da6c7808b251444dd0f6105
Wisen Tanasa <wisen@ceilfors.com>
2017-05-14 09:07:09 +0100

    Initial fix on bob.

---
+++ bob/bob.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class Bob {
+  hey(message) {
+    if (message.toUpperCase() === message) {
+      return 'Whoa, chill out!'
+    } else if (message.endsWith('?')) {
+      return 'Sure.'
+    }
+    return 'Whatever.'
+  }
+}
+
+export default Bob;
+
