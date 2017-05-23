c3c06ad6bf642dc5f9a1f40bd3082701476c2a40
Wisen Tanasa <wisen@ceilfors.com>
2017-05-23 06:55:53 +0100

    Fix beer song.

--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,14 +1,39 @@
+const eol = require('os').EOL
+
+function sing (start = 99, end = 0) {
+  let verses = ''
+  for (let i = start; i >= end; i--) {
+    verses += `${verse(i)}`
+    if (i !== end) {
+      verses += eol
+    }
+  }
+  return verses
+}
+
+function capitalize (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 function verse (n) {
-  const taken = n - 1 === 0 ? 'it' : 'one'
-  return `${beerBottles(n)} of beer on the wall, ${beerBottles(n)} of beer.
-Take ${taken} down and pass it around, ${beerBottles(n - 1)} of beer on the wall.
+  return `${capitalize(beerBottles(n))} of beer on the wall, ${beerBottles(n)} of beer.
+${act(n)}, ${beerBottles(n - 1)} of beer on the wall.
 `
 }
 
-function beerBottles (numOfBottles) {
-  const quantity = numOfBottles === 0 ? 'no more' : numOfBottles
-  const word = numOfBottles === 1 ? 'bottle' : 'bottles'
+function beerBottles (n) {
+  const quantity = n === 0 ? 'no more' : n === -1 ? 99 : n
+  const word = n === 1 ? 'bottle' : 'bottles'
   return `${quantity} ${word}`
 }
 
-export default { verse: verse }
+function act (n) {
+  if (n === 0) {
+    return 'Go to the store and buy some more'
+  } else {
+    const taken = n - 1 === 0 ? 'it' : 'one'
+    return `Take ${taken} down and pass it around`
+  }
+}
+
+export default { verse: verse, sing: sing }
