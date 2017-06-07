c9440d61cb1d3d0dc174447ba58102db9ff178c9
Wisen Tanasa <wisen@ceilfors.com>
2017-06-07 07:35:53 +0100

    Fix triangle.

---
+++ triangle/triangle.js
@@ -0,0 +1,36 @@
+const triangleType = {
+  1: 'equilateral',
+  2: 'isosceles',
+  3: 'scalene'
+}
+
+const sideCombinations = [
+  { sides: [0, 1], remaining: 2 },
+  { sides: [0, 2], remaining: 1 },
+  { sides: [1, 2], remaining: 0 }
+]
+
+class Triangle {
+  constructor (a, b, c) {
+    this.sides = [a, b, c]
+  }
+
+  validateLength () {
+    if (!this.sides.every(side => side > 0)) throw new Error('All sides must be more greater than 0')
+  }
+
+  validateInequality () {
+    if (!sideCombinations.every(c =>
+      this.sides[c.sides[0]] + this.sides[c.sides[1]] >= this.sides[c.remaining]
+    )) throw new Error('Violates triangle inequality')
+  }
+
+  kind () {
+    this.validateLength()
+    this.validateInequality()
+    const set = this.sides.reduce((set, side) => { set.add(side); return set }, new Set())
+    return triangleType[set.size]
+  }
+}
+
+export default Triangle
