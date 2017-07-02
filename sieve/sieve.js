e2807b9aa0527167826021caf7484be511d3d48b
Wisen Tanasa <wisen@ceilfors.com>
2017-07-02 09:05:21 +0100

    Refactor sieve.

--- sieve/sieve.js
+++ sieve/sieve.js
@@ -6,28 +6,20 @@ class Sieve {
   }
 
   get primes () {
-    const numbers = range(2, this.number)
-    const markers = Array(numbers.length)
-    // Mark numbers
-    for (let i = 0; i < numbers.length; i++) {
-      const marker = markers[i]
-      if (!marker) {
-        const base = numbers[i]
-        for (let j = i + 1; j < numbers.length; j++) {
-          if (numbers[j] % base === 0) {
-            markers[j] = true
-          }
-        }
-      }
+    const markPrime = (sieveNumbers, i = 0) => {
+      if (i === sieveNumbers.length - 1) return sieveNumbers
+      const newlyMarkedNumbers = sieveNumbers
+        .slice(i + 1, sieveNumbers.length)
+        .map(sn => ({ number: sn.number, prime: sn.prime && sn.number % sieveNumbers[i].number !== 0 }))
+      return markPrime(sieveNumbers.slice(0, i + 1).concat(newlyMarkedNumbers), ++i)
     }
-    // Collect numbers
-    const primeNumbers = []
-    for (let i = 0; i < numbers.length; i++) {
-      if (!markers[i]) {
-        primeNumbers.push(numbers[i])
-      }
-    }
-    return primeNumbers
+
+    const sieveNumbers = range(2, this.number)
+      .map(number => { return { number: number, prime: true } })
+
+    return markPrime(sieveNumbers)
+      .filter(sn => sn.prime)
+      .map(sn => sn.number)
   }
 }
 
