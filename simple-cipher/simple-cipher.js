8cf2afad67a0d72b21ec2c01c7cccb7d6efe1db5
Wisen Tanasa <wisen@ceilfors.com>
2017-07-12 07:08:02 +0100

    Refactor simple cipher.

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,17 +1,26 @@
 const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i)
+
 const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
 const shift = (char, shiftLength) => String.fromCharCode(char.charCodeAt(0) + shiftLength)
+
+const CHAR_CODE_A = 'a'.charCodeAt(0)
+const CHAR_CODE_Z = 'z'.charCodeAt(0)
+
 const wrap = char => {
-  const difference = char.charCodeAt(0) - 'z'.charCodeAt(0) - 1
-  return String.fromCharCode('a'.charCodeAt(0) + difference)
+  const difference = char.charCodeAt(0) - CHAR_CODE_Z - 1
+  return String.fromCharCode(CHAR_CODE_A + difference)
 }
+
 const unwrap = char => {
-  const difference = 'a'.charCodeAt(0) - char.charCodeAt(0) - 1
-  return String.fromCharCode('z'.charCodeAt(0) - difference)
+  const difference = CHAR_CODE_A - char.charCodeAt(0) - 1
+  return String.fromCharCode(CHAR_CODE_Z - difference)
 }
+
 const randomKey = length =>
   range(0, length)
-    .map(item => String.fromCharCode(getRandomIntInclusive('a'.charCodeAt(0), 'z'.charCodeAt(0))))
+    .map(_ => getRandomIntInclusive(CHAR_CODE_A, CHAR_CODE_Z))
+    .map(code => String.fromCharCode(code))
     .join('')
 
 export default class Cipher {
@@ -23,7 +32,7 @@ export default class Cipher {
   }
 
   shiftDistance (index) {
-    return this.key.charCodeAt(index) - 'a'.charCodeAt(0)
+    return this.key.charCodeAt(index) - CHAR_CODE_A
   }
 
   encode (input) {
