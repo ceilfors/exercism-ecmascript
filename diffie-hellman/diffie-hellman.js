746145b3925e4e95055954aa4b77a2c8375062c6
Wisen Tanasa <wisen@ceilfors.com>
2017-07-14 07:08:54 +0100

    Fix diffie hellman.

---
+++ diffie-hellman/diffie-hellman.js
@@ -0,0 +1,25 @@
+
+const isPrime = value => {
+  for (var i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false
+    }
+  }
+  return value > 1
+}
+const assert = require('assert')
+
+export default class DiffieHellman {
+  constructor (p, g) {
+    assert(isPrime(p), 'p is not prime')
+    assert(isPrime(g), 'g is not prime')
+    this.p = p
+    this.g = g
+  }
+
+  getPublicKeyFromPrivateKey (privateKey) {
+    assert(privateKey > 1, 'private key must be greater than 1')
+    assert(privateKey < this.p, 'private key must be less than p')
+    return this.g ** privateKey % this.p
+  }
+}
