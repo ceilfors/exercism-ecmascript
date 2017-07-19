3658e2c8bdb9d9fcb6e6875bb681edf414c87620
Wisen Tanasa <wisen@ceilfors.com>
2017-07-19 07:08:51 +0100

    Initial fix on pythagorean-triplet.

---
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,23 @@
+export default class Triplet {
+  constructor (a, b, c) {
+    this.a = a
+    this.b = b
+    this.c = c
+  }
+
+  sum () {
+    return this.a + this.b + this.c
+  }
+
+  product () {
+    return this.a * this.b * this.c
+  }
+
+  isPythagorean () {
+    return this.a ** 2 + this.b ** 2 === this.c ** 2
+  }
+
+  // static where () {
+  //   return [new this(1, 2, 3)]
+  // }
+}
