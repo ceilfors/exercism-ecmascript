8d3a4c0a679a5499ad32b7a43717b2c876d13d9b
Wisen Tanasa <wisen@ceilfors.com>
2017-07-21 07:21:46 +0100

    Fix pythagorean triplet.

--- pythagorean-triplet/pythagorean-triplet.js
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -1,3 +1,13 @@
+function * combination (min, max) {
+  for (let a = min; a <= max; a++) {
+    for (let b = a + 1; b <= max; b++) {
+      for (let c = b + 1; c <= max; c++) {
+        yield [a, b, c]
+      }
+    }
+  }
+}
+
 export default class Triplet {
   constructor (a, b, c) {
     this.a = a
@@ -17,18 +27,10 @@ export default class Triplet {
     return this.a ** 2 + this.b ** 2 === this.c ** 2
   }
 
-  static where ({maxFactor}) {
-    const triplets = []
-    for (let a = 0; a <= maxFactor; a++) {
-      for (let b = a + 1; b <= maxFactor; b++) {
-        for (let c = b + 1; c <= maxFactor; c++) {
-          const triplet = new this(a, b, c)
-          if (triplet.isPythagorean()) {
-            triplets.push(triplet)
-          }
-        }
-      }
-    }
-    return triplets
+  static where ({minFactor = 0, maxFactor, sum}) {
+    return Array.from(combination(minFactor, maxFactor))
+      .map(([a, b, c]) => new this(a, b, c))
+      .filter(triplet => triplet.isPythagorean())
+      .filter(triplet => sum ? triplet.sum() === sum : true)
   }
 }
