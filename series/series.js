1cd4aada0b10e80ea4987dbebfa65f30ce0b109d
Wisen Tanasa <wisen@ceilfors.com>
2017-07-29 07:04:13 +0100

    Fix series.

--- series/series.js
+++ series/series.js
@@ -6,4 +6,11 @@ export default class Series {
   get digits () {
     return [...this.string].map(digit => Number(digit))
   }
+
+  slices (size) {
+    if (size > this.digits.length) throw new Error('Slice size is too big.')
+    return this.digits
+      .map((value, index, array) => (index > array.length - size) ? null : array.slice(index, index + size))
+      .filter(slice => slice !== null)
+  }
 }
